a1f79a45b9fd0d1be87598508bcbabe97984e215
Bagus Wicaksono <baguswicaksono388@gmail.com>
2024-12-05 18:56:43 +0700

    upload project

--- swagger.js
+++ swagger.js
@@ -1,30 +1,30 @@
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Contract SIMS PPOB',
-      version: '1.0.0',
-      description: 'Documentation for Take Home Test API',
+      title: "API Contract SIMS PPOB",
+      version: "1.0.0",
+      description: "Documentation for Take Home Test API",
     },
     servers: [
       {
-        url: 'http://localhost:5000',
-        description: 'Local server',
+        url: process.env.SERVER_URL || "http://localhost:5000",
+        description: "Local server",
       },
     ],
     components: {
       securitySchemes: {
         bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
     },
   },
-  apis: ['./routes/*.js'],
+  apis: ["./routes/*.js"],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
