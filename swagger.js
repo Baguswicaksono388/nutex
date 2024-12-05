fedd37fd3fa99777075d2019efee34ff40d4d8f7
Bagus Wicaksono <baguswicaksono388@gmail.com>
2024-12-05 18:59:45 +0700

    upload project

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: process.env.SERVER_URL || "http://localhost:5000",
+        url: "https://nutex-production.up.railway.app",
         description: "Local server",
       },
     ],
